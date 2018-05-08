e5a680d3cf09d5150ecd91aecc7834d5e9145483
ArielWolffe <arielwolffe@gmail.com>
2018-05-08 19:07:21 -0400

    Website updates

---
+++ scripts/animate.js
@@ -0,0 +1,15 @@
+// var div = document.getElementById("numbers");
+// var container = document.getElementById("headerContainer");
+
+// var me = function(event) {
+//   var x = event.clientX,
+//     y = event.clientY,
+//     w = div.offsetWidth,
+//     m = 30,
+//     square = div.getBoundingClientRect(),
+//     pxToBox = x - (w / 2 - square.left),
+//     left = m * pxToBox / this.offsetWidth;
+//   div.style.left = left + "px";
+// };
+
+// container.addEventListener("mousemove", me, false);
--- scripts/stickynav.js
+++ scripts/stickynav.js
@@ -1,6 +1,6 @@
 // <!-- on-scroll Javascript to make top-nav white -->
 window.onscroll = function() {
-  myFunction();
+  solidNav();
 };
 
 var header = document.getElementById("top-navigation");
@@ -9,7 +9,7 @@ var activeMenu = document.getElementById("d-navactive");
 
 var sticky = header.offsetTop;
 
-function myFunction() {
+function solidNav() {
   if (window.pageYOffset > sticky) {
     header.classList.add("d-top-nav--solid");
     menu.classList.add("d-menu--solid");
@@ -18,5 +18,14 @@ function myFunction() {
     header.classList.remove("d-top-nav--solid");
     menu.classList.remove("d-menu--solid");
     activeMenu.classList.remove("d-menu--solid--active");
+  }
+}
+
+function toggleMenu() {
+  var x = document.getElementById("m-navmenu");
+  if (x.classList.contains("m-menu")) {
+    x.classList.remove("m-menu");
+  } else {
+    x.classList.add("m-menu");
   }
 }
