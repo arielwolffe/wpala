57a6d77043c193160d13c08f089bd41b9955c45c
ArielWolffe <arielwolffe@gmail.com>
2018-05-10 14:39:03 -0400

    website updates

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,15 +1,15 @@
-// var div = document.getElementById("numbers");
-// var container = document.getElementById("headerContainer");
+var div = document.getElementById("numbers");
+var container = document.getElementById("headerContainer");
 
-// var me = function(event) {
-//   var x = event.clientX,
-//     y = event.clientY,
-//     w = div.offsetWidth,
-//     m = 30,
-//     square = div.getBoundingClientRect(),
-//     pxToBox = x - (w / 2 - square.left),
-//     left = m * pxToBox / this.offsetWidth;
-//   div.style.left = left + "px";
-// };
+var me = function(event) {
+  var x = event.clientX,
+    y = event.clientY,
+    w = div.offsetWidth,
+    m = 30,
+    square = div.getBoundingClientRect(),
+    pxToBox = x - w / 2,
+    left = m * pxToBox / this.innerWidth;
+  div.style.left = left + "px";
+};
 
-// container.addEventListener("mousemove", me, false);
+container.addEventListener("mousemove", me, false);
