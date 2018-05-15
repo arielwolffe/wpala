ffed8fa8cf4f41385996ca95237f30906a71415c
ArielWolffe <arielwolffe@gmail.com>
2018-05-15 08:48:59 -0400

    added updates

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,15 +1,28 @@
-var div = document.getElementById("numbers");
-var container = document.getElementById("headerContainer");
+!(function($doc, $win) {
+  var screenWidth = $win.screen.width / 2,
+    screenHeight = $win.screen.height / 2,
+    $elems = $doc.getElementsByClassName("d-header__numbers"),
+    validPropertyPrefix = "",
+    otherProperty = "perspective(1000px)",
+    elemStyle = $elems[0].style;
 
-var me = function(event) {
-  var x = event.clientX,
-    y = event.clientY,
-    w = div.offsetWidth,
-    m = 30,
-    square = div.getBoundingClientRect(),
-    pxToBox = x - w / 2,
-    left = m * pxToBox / this.innerWidth;
-  div.style.left = left + "px";
-};
+  if (typeof elemStyle.webkitTransform == "string") {
+    validPropertyPrefix = "webkitTransform";
+  } else if (typeof elemStyle.MozTransform == "string") {
+    validPropertyPrefix = "MozTransform";
+  }
 
-container.addEventListener("mousemove", me, false);
+  $doc.addEventListener("mousemove", function(e) {
+    var centroX = e.clientX - screenWidth,
+      centroY = screenHeight - (e.clientY + 15),
+      degX = centroX * 0.06,
+      degY = centroY * 0.04,
+      $elem;
+
+    for (var i = 0; i < $elems.length; i++) {
+      $elem = $elems[i];
+      $elem.style[validPropertyPrefix] =
+        otherProperty + "rotateY(" + degX + "deg)  rotateX(" + degY + "deg)";
+    }
+  });
+})(document, window);
